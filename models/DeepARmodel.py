ad2a22fca879bf3c28c1699bd3ee217050a6854c
sy2657 <60480017+sy2657@users.noreply.github.com>
2020-05-15 15:16:02 -0700

    Update DeepARmodel.py

--- models/DeepARmodel.py
+++ models/DeepARmodel.py
@@ -2,5 +2,5 @@ from gluonts.model.deepar import DeepAREstimator
 from gluonts.trainer import Trainer
 # input: training data ListDataset : training_data
 
-estimator = DeepAREstimator(freq="1H", prediction_length=30, trainer=Trainer(epochs=10,batch_size=10))
+estimator = DeepAREstimator(freq="1H", prediction_length=30, trainer=Trainer(epochs=10,batch_size=10), cell_type='gru')
 predictor = estimator.train(training_data= training_data ) 
