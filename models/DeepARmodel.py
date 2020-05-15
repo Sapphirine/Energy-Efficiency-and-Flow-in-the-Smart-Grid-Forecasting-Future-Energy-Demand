62103c41a86c654dfbefbd32c7cbb79ab90592a5
sy2657 <60480017+sy2657@users.noreply.github.com>
2020-05-15 14:49:02 -0700

    Create DeepARmodel.py

---
+++ models/DeepARmodel.py
@@ -0,0 +1,6 @@
+from gluonts.model.deepar import DeepAREstimator
+from gluonts.trainer import Trainer
+# input: training data ListDataset : training_data
+
+estimator = DeepAREstimator(freq="1H", prediction_length=30, trainer=Trainer(epochs=10,batch_size=10))
+predictor = estimator.train(training_data= training_data ) 
