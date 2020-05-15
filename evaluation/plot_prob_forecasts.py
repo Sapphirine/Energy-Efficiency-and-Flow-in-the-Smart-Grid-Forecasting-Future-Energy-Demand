ac6ab07c9a0c8b049a6f4a5e324a49bbb97ea720
sy2657 <60480017+sy2657@users.noreply.github.com>
2020-05-15 14:58:14 -0700

    Create plot_prob_forecasts.py

---
+++ evaluation/plot_prob_forecasts.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+def plot_prob_forecasts(ts_entry, forecast_entry):
+    plot_length = 150
+    prediction_intervals = (50.0, 90.0) # change the interval values 
+    legend = ["observations", "median prediction"] + [f"{k}% prediction interval" for k in prediction_intervals][::-1]
+
+    fig, ax = plt.subplots(1, 1, figsize=(10, 7))
+    ts_entry[-plot_length:].plot(ax=ax)  # plot the time series
+    forecast_entry.plot(prediction_intervals=prediction_intervals, color='g')
+    plt.grid(which="both")
+    plt.legend(legend, loc="upper left")
+    plt.show()
