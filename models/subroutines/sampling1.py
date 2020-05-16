8f3a0352aca7a3a0436c5cb543f099b390bdda38
sy2657 <60480017+sy2657@users.noreply.github.com>
2020-05-15 17:30:45 -0700

    Create sampling1.py

---
+++ models/subroutines/sampling1.py
@@ -0,0 +1,25 @@
+# orig sampling method from unsupervised rep learning
+def sampling1(y, N, K): # sequence y_i for i in 1 to N
+  # size of y
+  for i in range(1,N):
+    yi = y[i]
+    ysize = len(yi)
+    # pick sizes s_pos and s_ref - unif at random 
+    spos = np.random.randint(1, ysize)
+    sref = np.random.randint(spos, ysize) 
+    # pick xref, xpos 
+    # pick starting index among 0 to (ysize - sref)
+    startref = np.random.randint(0, ysize - sref)
+    startpos = np.random.randint(0, ysize - spos)
+    xref = yi[startref:startref+sref]
+    xpos = yi[startpos:startpos+spos]
+    xneg= []
+    for k in range(1, K): 
+      ik = np.random.randint(1, N)
+      sneg = np.random.randint(1,len(y[ik]))
+      # pick xneg
+      startneg = np.random.randint(0, ysize-sneg)
+      yk = y[k]
+      xneg_k = yk[startneg:sneg]
+      xneg.append(xneg_k)
+    return( xref, xpos, xneg)
